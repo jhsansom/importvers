89b7544410b8b58444fc9eb60bad3c44782655c0
jhsansom <jhsansom@gmail.com>
2020-06-16 18:24:59 -0500

    added comments

--- importvers/importvers.py
+++ importvers/importvers.py
@@ -3,24 +3,44 @@ import os, sys
 import importlib
 import shutil
 
-def importvers(repo_path, module_name, vers, tmp_path='.'):
+def importvers(repo_path, module_name, vers, tmp_path='./.tmp'):
+    """Returns working tree version
+
+	Parameters
+	----------
+	repo_path : str
+        Filepath to repository (relative or absolute)
+    module_name : str
+        Using normal Python import syntax, this is the module you wish to import from the package
+    vers : str
+        Either the hash or tag of the commit
+    tmp_path : str
+        Path to the temporary folder created for the working tree
+
+	Returns
+	----------
+	module
+
+	"""
+
+    # finds the absolute path of tmp_path
     tmp_path = os.path.abspath(tmp_path)
 
     # add worktree for the version in question for this test
     repo = Repo(repo_path)
-    repo.git.worktree('add', tmp_path+'/.tmp', vers)
+    repo.git.worktree('add', tmp_path, vers)
 
     # import path
     try:
-        module = importpath(tmp_path+'/.tmp', module_name)
+        module = importpath(tmp_path, module_name)
     except:
         # if import fails, prune working tree before returning exception
-        shutil.rmtree(tmp_path+'/.tmp')
+        shutil.rmtree(tmp_path)
         repo.git.worktree('prune')
         raise
 
     # switch repository back
-    shutil.rmtree(tmp_path+'/.tmp')
+    shutil.rmtree(tmp_path)
     repo.git.worktree('prune')
 
     return module
