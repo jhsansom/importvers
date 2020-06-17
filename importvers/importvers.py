dcfbe28dac81da08687b9611615ae4f2cdece124
jhsansom <jhsansom@gmail.com>
2020-06-17 00:21:44 -0500

    debugged code and created example notebook

--- importvers/importvers.py
+++ importvers/importvers.py
@@ -2,6 +2,7 @@ from git import Repo
 import os, sys
 import importlib
 import shutil
+import copy
 
 def importvers(repo_path, module_name, vers, tmp_path='./.tmp'):
     """Returns working tree version
@@ -24,7 +25,7 @@ def importvers(repo_path, module_name, vers, tmp_path='./.tmp'):
 	"""
 
     # finds the absolute path of tmp_path
-    tmp_path = os.path.abspath(tmp_path)
+    tmp_path = os.path.abspath(tmp_path+'_'+vers)
 
     # add worktree for the version in question for this test
     repo = Repo(repo_path)
@@ -43,12 +44,26 @@ def importvers(repo_path, module_name, vers, tmp_path='./.tmp'):
     shutil.rmtree(tmp_path)
     repo.git.worktree('prune')
 
+    # rename to differentiate modules from one another
+    module.__name__ = module_name+'_'+vers
+
+    # renames module to unique name
+    sys.modules[module_name+'_'+vers] = module
+
+    # removes module with undifferentiated name so another version can be loaded if necessary
+    sys.modules.pop(module_name)
+
     return module
 
 
-def importpath(path, module):
+def importpath(path, module_name):
     # import local file
     sys.path.append(path)
 
     # import path
-    return importlib.import_module(module)
+    module = importlib.import_module(module_name)
+
+    # removes from path b/c no longer necessary
+    sys.path.remove(path)
+
+    return module
